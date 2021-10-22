547a07d21bfd1b140ea7f9e1e19a5c034d6aead6
Wiliane Souza <wiliane_souza@hotmail.com>
2021-10-22 10:53:45 -0300

    adicionado código do material

--- src/main/java/br/upe/mavenBasico/App.java
+++ src/main/java/br/upe/mavenBasico/App.java
@@ -8,6 +8,9 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        String[] resultado = ResultadoMegasena.obtemUltimoResultado();
+        for (String dezena: resultado) {
+            System.out.println( dezena + "" );
+        }
     }
 }
