542cac49d2f01ce0a6c297c2920816f7eee922bc
Wiliane Souza <wiliane_souza@hotmail.com>
2021-10-25 14:54:09 -0300

    removidos arquivos não usados e adicionado rascunho dos testes

--- src/test/java/br/upe/mavenBasico/ResultadoMegasenaTest.java
+++ src/test/java/br/upe/mavenBasico/ResultadoMegasenaTest.java
@@ -9,12 +9,42 @@ public class ResultadoMegasenaTest extends TestCase {
     /** Número de dezenas esperadas no resultado da mega-sena. */
     private final static int NUMERO_DE_DEZENAS = 6;
 
-    /**  Teste do método obtemUltimoResultado() */
+    /**  Teste para resultado da MegaSena */
     @Test
-    public void testObtemUltimoResultado() {
-        List<String> ultimoResultado =
-                ResultadoMegasena.obtemUltimoResultado();
-        assertNotNull(ultimoResultado);
-        assertTrue( ultimoResultado.toArray().length == NUMERO_DE_DEZENAS );
+    public void testResultadoMegaSena() {
+        List<List<String>> resultadoMegaSena = new Scrapper(
+                "http://loterias.caixa.gov.br/wps/portal/loterias/landing/megasena/",
+                QuantidadeDezenas.MEGASENA
+        ).pegarResultado();
+        assertNotNull(resultadoMegaSena);
+        for(List<String> dezenas: resultadoMegaSena) {
+            assertTrue( resultadoMegaSena.toArray().length == QuantidadeDezenas.MEGASENA.getValor() );
+        }
+    }
+
+    /**  Teste para resultado da Quina */
+    @Test
+    public void testResultadoQuina() {
+        List<List<String>> resultadoMegaSena = new Scrapper(
+                "http://loterias.caixa.gov.br/wps/portal/loterias/landing/quina/",
+                QuantidadeDezenas.QUINA
+        ).pegarResultado();
+        assertNotNull(resultadoMegaSena);
+        for(List<String> dezenas: resultadoMegaSena) {
+            assertTrue( resultadoMegaSena.toArray().length ==QuantidadeDezenas.QUINA.getValor() );
+        }
+    }
+
+    /**  Teste para resultado da  */
+    @Test
+    public void testResultadoLotomania() {
+        List<List<String>> resultadoMegaSena = new Scrapper(
+                "http://loterias.caixa.gov.br/wps/portal/loterias/landing/lotomania/",
+                QuantidadeDezenas.LOTOMANIA
+        ).pegarResultado();
+        assertNotNull(resultadoMegaSena);
+        for(List<String> dezenas: resultadoMegaSena) {
+            assertTrue( resultadoMegaSena.toArray().length == QuantidadeDezenas.LOTOMANIA.getValor() );
+        }
     }
 }
